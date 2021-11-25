c214a49d0a9812b2c919e4e19c48be27386ae901
Константин Бедрицкий <kmb2@tpu.ru>
2021-11-26 02:32:34 +0600

    Arithmetic and bitwise operations

--- src/cpu.rs
+++ src/cpu.rs
@@ -10,6 +10,7 @@ pub struct Cpu {
     stack: Stack,
 }
 
+#[derive(Debug)]
 struct InstructionData {
     instruction: u16,
     nnn: u16,
@@ -34,6 +35,7 @@ impl Cpu {
     pub fn run(&mut self, bus: &mut Bus) {
         let instruction = self.fetch_instruction(bus);
         let params = Self::parse_instruction(instruction);
+        println!("Params: {:?}", params);
         self.execute(bus, params);
     }
 
@@ -84,12 +86,85 @@ impl Cpu {
                 self.pc += 2;
             }
             0x6 => {
-                // LD vx, kk
-                self.vx[params.x as usize] = params.kk;
+                // LD vx, byte
+                self.write_reg(params.x, params.kk);
+                self.pc += 2;
+            }
+            0x7 => {
+                // ADD vx, byte
+                self.write_reg(params.x, self.vx[params.x as usize].wrapping_add(params.kk));
+                self.pc += 2;
+            }
+            0x8 => {
+                let vx = self.vx[params.x as usize];
+                let vy = self.vx[params.y as usize];
+                match params.n {
+                    0x0 => {
+                        // LD vx, vy
+                        self.write_reg(params.x, vy);
+                    }
+                    0x1 => {
+                        // OR vx, vy
+                        println!("vx = {:2x?}, vy = {:2x?}, or = {:2x?}", vx, vy, vx | vy);
+                        self.write_reg(params.x, vx | vy);
+                    }
+                    0x2 => {
+                        // AND vx, vy
+                        self.write_reg(params.x, vx & vy);
+                    }
+                    0x3 => {
+                        // XOR vx, vy
+                        self.write_reg(params.x, vx ^ vy);
+                    }
+                    0x4 => {
+                        // ADD vx, vy
+                        let sum = vx as u16 + vy as u16;
+                        self.write_reg(params.x, sum as u8);
+                        if sum > 0xff {
+                            self.write_reg(0xf, 1);
+                        }
+                    }
+                    0x5 => {
+                        // SUB vx, vy
+                        if vx > vy {
+                            self.write_reg(0xf, 1);
+                        } else {
+                            self.write_reg(0xf, 0);
+                        }
+
+                        self.write_reg(params.x, vx.wrapping_sub(vy));
+                    }
+                    0x6 => {
+                        // SHR vx
+                        self.write_reg(0xf, vx & 0x1);
+                        self.write_reg(params.x, vx >> 1);
+                    }
+                    0x7 => {
+                        // SUBN vx, vy
+                        if vy > vx {
+                            self.write_reg(0xf, 1);
+                        } else {
+                            self.write_reg(0xf, 0);
+                        }
+
+                        self.write_reg(params.x, vy.wrapping_sub(vx));
+                    }
+                    0xe => {
+                        // SHL vx
+                        self.write_reg(0xf, (vx & 0x80) >> 7);
+                        self.write_reg(params.x, vx << 1);
+                    }
+                    _ => {}
+                }
+
                 self.pc += 2;
             }
             _ => todo!(),
         }
+    }
+
+    fn write_reg(&mut self, x: u8, value: u8) {
+        self.vx[x as usize] = value;
     }
 
     fn fetch_instruction(&mut self, bus: &mut Bus) -> u16 {
@@ -218,5 +293,132 @@ mod tests {
         assert_eq!(cpu.pc, 0x0214);
         assert_eq!(cpu.vx[1], 0xc1);
         assert_eq!(cpu.vx[5], 0xc1);
+    }
+
+    #[test]
+    fn inst_0x7xkk() {
+        let mut cpu = Cpu::new();
+        let mut bus = Bus::new();
+
+        bus.write_ram(
+            &[
+                0x71, 0x20, // 0x0200: add v1 0x20
+                0x71, 0x20, // 0x0202: add v1 0x20
+                0x71, 0xE0, // 0x0204: add v1 0xe0
+                0x72, 0x01, // 0x0206: add v2 0x01
+            ],
+            ENTRY_POINT,
+        );
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x20);
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x40);
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x20);
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[2], 0x01);
+    }
+
+    #[test]
+    fn inst_0x8xyn() {
+        let mut cpu = Cpu::new();
+        let mut bus = Bus::new();
+
+        bus.write_ram(
+            &[
+                0x61, 0xF4, // 0x0200: ld v1, 0xf4  (v1 = 0b11110100)
+                0x62, 0x82, // 0x0202: ld v2, 0x82  (v2 = 0b10000010)
+                0x81, 0x21, // 0x0204: or v1, v2    (v1 = 0b11110110)
+                0x81, 0x22, // 0x0206: and v1, v2   (v1 = 0b10000010)
+                0x62, 0x11, // 0x0208: ld v2, 0x11  (v2 = 0b00010001)
+                0x81, 0x20, // 0x020a: ld v1, v2    (v1 = 0b00010001)
+                0x62, 0x85, // 0x020c: ld v2, 0x85  (v2 = 0b10000101)
+                0x81, 0x23, // 0x020e: xor v1, v2   (v1 = 0b10010100)
+                0x81, 0x24, // 0x0210: add v1, v2   (v1 = 0b00011001)
+                0x81, 0x24, // 0x0212: add v1, v2   (v1 = 0b10011110)
+                0x81, 0x25, // 0x0214: sub v1, v2   (v1 = 0b00011001)
+                0x81, 0x25, // 0x0216: sub v1, v2   (v1 = 0b10010100)
+                0x81, 0x06, // 0x0218: shr v1       (v1 = 0b01001010)
+                0x82, 0x06, // 0x021a: shr v2       (v2 = 0b01000010)
+                0x81, 0x27, // 0x021c: subn v1, v2  (v1 = 0b11111000)
+                0x81, 0x0e, // 0x021e: shl v1       (v1 = 0b11110000)
+                0x81, 0x06, // 0x0218: shr v1       (v1 = 0b01111000)
+                0x81, 0x0e, // 0x021e: shl v1       (v1 = 0b11110000)
+            ],
+            ENTRY_POINT,
+        );
+
+        cpu.run(&mut bus);
+        cpu.run(&mut bus);
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0xf6);
+        assert_eq!(cpu.vx[2], 0x82);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x82);
+        assert_eq!(cpu.vx[2], 0x82);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x82);
+        assert_eq!(cpu.vx[2], 0x11);
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x11);
+        assert_eq!(cpu.vx[2], 0x11);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[2], 0x85);
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x94);
+        assert_eq!(cpu.vx[0xf], 0x0);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x19);
+        assert_eq!(cpu.vx[2], 0x85);
+        assert_eq!(cpu.vx[0xf], 0x1);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x9e);
+        assert_eq!(cpu.vx[2], 0x85);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x19);
+        assert_eq!(cpu.vx[2], 0x85);
+        assert_eq!(cpu.vx[0xf], 0x1);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x94);
+        assert_eq!(cpu.vx[2], 0x85);
+        assert_eq!(cpu.vx[0xf], 0x0);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x4a);
+        assert_eq!(cpu.vx[2], 0x85);
+        assert_eq!(cpu.vx[0xf], 0x0);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x4a);
+        assert_eq!(cpu.vx[2], 0x42);
+        assert_eq!(cpu.vx[0xf], 0x1);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0xf8);
+        assert_eq!(cpu.vx[2], 0x42);
+        assert_eq!(cpu.vx[0xf], 0x0);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0xf0);
+        assert_eq!(cpu.vx[2], 0x42);
+        assert_eq!(cpu.vx[0xf], 0x1);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0x78);
+        assert_eq!(cpu.vx[2], 0x42);
+        assert_eq!(cpu.vx[0xf], 0x0);
+
+        cpu.run(&mut bus);
+        assert_eq!(cpu.vx[1], 0xf0);
+        assert_eq!(cpu.vx[2], 0x42);
+        assert_eq!(cpu.vx[0xf], 0x0);
     }
 }
