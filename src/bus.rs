7b95b9a2cd6d1ac923c774a02748b7cb1961386f
Константин Бедрицкий <kmb2@tpu.ru>
2021-11-29 21:18:07 +0600

    Keyboard abstraction

--- src/bus.rs
+++ src/bus.rs
@@ -29,11 +29,15 @@ impl Bus {
     }
 
     pub fn get_key_pressed(&self) -> Option<u8> {
-        todo!()
+        self.keyboard.get_key_pressed()
     }
 
-    pub fn is_key_pressed(&self, _key: u8) -> bool {
-        todo!()
+    pub fn is_key_pressed(&self, key: u8) -> bool {
+        self.keyboard.is_key_pressed(key)
+    }
+
+    pub fn set_key_pressed(&mut self, key: Option<u8>) {
+        self.keyboard.set_key_pressed(key)
     }
 
     pub fn draw(&mut self, _x: u8, _y: u8, _data: u8) -> bool {
--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,9 +1,58 @@
 pub struct Keyboard {
-    // TODO
+    key_pressed: Option<u8>,
 }
 
 impl Keyboard {
     pub fn new() -> Self {
-        Self {}
+        Self { key_pressed: None }
+    }
+
+    pub fn set_key_pressed(&mut self, key: Option<u8>) {
+        self.key_pressed = key;
+    }
+
+    pub fn get_key_pressed(&self) -> Option<u8> {
+        self.key_pressed
+    }
+
+    pub fn is_key_pressed(&self, key: u8) -> bool {
+        self.key_pressed.map_or(false, |pressed| pressed == key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_key_pressed() {
+        let expected = Some(4);
+        let keyboard = Keyboard {
+            key_pressed: expected,
+        };
+
+        let actual = keyboard.get_key_pressed();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn set_key_pressed() {
+        let mut keyboard = Keyboard::new();
+        assert_eq!(keyboard.get_key_pressed(), None);
+
+        let expected = Some(4);
+        keyboard.set_key_pressed(expected);
+        let actual = keyboard.get_key_pressed();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn is_key_pressed() {
+        let keyboard = Keyboard {
+            key_pressed: Some(2),
+        };
+
+        assert!(keyboard.is_key_pressed(2));
+        assert!(!keyboard.is_key_pressed(5));
     }
 }
